acdc9b44acffd62eb052d883d0612a0aa7e25b70
Pavel <Pikcelll@yandex-team.ru>
2022-10-01 16:25:48 +0300

    Удаляю node modules

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import { Pages } from "./pages/index";
 
 import "./assets/style/index.scss";
+
 import "./store/init";
 
 ReactDOM.render(<Pages />, document.getElementById("root"));
