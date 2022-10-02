37a6cfc00a956619835a07ab2c50b237f8e81438
egor-kuch <egor-kuch@yandex-team.ru>
2022-10-02 10:52:33 +0300

    f

--- client/src/pages/JournalPage/JournalPage.tsx
+++ client/src/pages/JournalPage/JournalPage.tsx
@@ -1,25 +1,26 @@
 import b_ from 'b_';
 import * as faceapi from 'face-api.js';
 import React from 'react';
-import { emotionsStore } from '../../api';
-import { postEmotions } from '../../store/emotions';
 
 import bg2 from '../../assets/icons/bg2.png';
-import './JournalPage.scss';
+import button from '../../assets/icons/button.png';
+import buttonCr from '../../assets/icons/buttonCr.png';
 import { AboutEmotionSection } from '../../components/AboutEmotionSection/AboutEmotionSection';
+import { postEmotions } from '../../store/emotions';
+
+import './JournalPage.scss';
 
 const b = b_.with('journal-page');
 
 export const JournalPage = () => {
+    const [modelsLoaded, setModelsLoaded] = React.useState(false);
+    const [captureVideo, setCaptureVideo] = React.useState(false);
+    const [values, setValues] = React.useState({});
     const [videoLoaded, setVideoLoaded] = React.useState(false);
-    const [values, setValues] = React.useState<
-        emotionsStore & { fearful: number }
-        //@ts-ignore
-    >({});
 
     const videoRef = React.useRef();
-    const videoHeight = 480;
-    const videoWidth = 640;
+    const videoHeight = 300;
+    const videoWidth = 300;
     const canvasRef = React.useRef();
 
     React.useEffect(() => {
@@ -37,18 +38,9 @@ export const JournalPage = () => {
         loadModels();
     }, []);
 
-    const closeWebcam = () => {
-        //@ts-ignore
-        videoRef.current.pause();
-        //@ts-ignore
-
-        videoRef.current.srcObject.getTracks()[0].stop();
-        setVideoLoaded(true);
-
-        postEmotions(values);
-    };
-
     const startVideo = () => {
+        setValues({});
+        setCaptureVideo(true);
         navigator.mediaDevices
             .getUserMedia({ video: { width: 300 } })
             .then((stream) => {
@@ -65,20 +57,19 @@ export const JournalPage = () => {
 
         setTimeout(() => {
             closeWebcam();
-        }, 3000);
+        }, 10000);
     };
 
     const handleVideoOnPlay = () => {
         const id = setInterval(async () => {
             if (canvasRef && canvasRef.current) {
-                const result = await faceapi.createCanvasFromMedia(
+                //@ts-ignore
+
+                canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(
                     //@ts-ignore
 
                     videoRef.current,
                 );
-
-                //@ts-ignore
-                canvasRef.current.innerHTML = result;
                 const displaySize = {
                     width: videoWidth,
                     height: videoHeight,
@@ -107,6 +98,7 @@ export const JournalPage = () => {
                         'sad',
                         'angry',
                         'fearful',
+                        'disgusted',
                         'surprised',
                     ]
                         .map(function (expression) {
@@ -135,17 +127,6 @@ export const JournalPage = () => {
                                     (v?.[expr.expression] ?? 0) + 1,
                             };
                         });
-                        setValues((v) => ({
-                            ...v,
-
-                            //@ts-ignore
-                            ['surprise']: v.surprised ?? 0,
-                        }));
-                        setValues((v) => ({
-                            ...v,
-                            //@ts-ignore
-                            ['fear']: v.fearful ?? 0,
-                        }));
                     });
                 });
 
@@ -173,6 +154,18 @@ export const JournalPage = () => {
         }, 1000);
     };
 
+    const closeWebcam = () => {
+        //@ts-ignore
+
+        videoRef.current.pause();
+        //@ts-ignore
+
+        videoRef.current.srcObject.getTracks()[0].stop();
+        setCaptureVideo(false);
+
+        postEmotions(values as any);
+    };
+
     return (
         <section
             style={{ background: `url(${bg2}) center center/cover no-repeat` }}
@@ -180,7 +173,49 @@ export const JournalPage = () => {
         >
             <div>
                 <h1 className={b('h1')}>Запечатлите свою эмоцию</h1>
-             
+                {captureVideo && modelsLoaded ? (
+                    <div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                padding: '10px',
+                            }}
+                        >
+                            <video
+                                //@ts-ignore
+
+                                ref={videoRef}
+                                height={videoHeight}
+                                width={videoWidth}
+                                onPlay={handleVideoOnPlay}
+                                style={{ borderRadius: '10px' }}
+                            />
+                            <canvas
+                                //@ts-ignore
+
+                                ref={canvasRef}
+                                style={{ position: 'absolute' }}
+                            />
+                        </div>
+                    </div>
+                ) : (
+                    <div className={b('wrapper')}>
+                        <div
+                            style={{
+                                background: `url(${button}) center center/cover no-repeat`,
+                                height: '500px',
+                            }}
+                        ></div>
+                        <div
+                            style={{
+                                background: `url(${buttonCr}) center center/cover no-repeat`,
+                            }}
+                            onClick={startVideo}
+                            className={b('btn')}
+                        ></div>
+                    </div>
+                )}
                 <h3 className={b('h3')}>
                     Мы определим ее и покажем по ней статистику, а также поможем
                     детально узнать себя, свои эмоции и научим управлять ими
